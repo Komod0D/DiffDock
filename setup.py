02e425ac52244893c755946b387263a36f692c12
Kimon Protopapas <kimon.protopapas@gmail.com>
2026-01-20 12:27:05 +0100

    reformat as package

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='diffdock',
+      version='1.0',
+      description='DiffDock: Diffusion Steps, Twists, and Turns for Molecular Docking',
+      author='Gabriele Corso',
+      packages=['diffdock'],
+     )
